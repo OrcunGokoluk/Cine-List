e0aa8b38354bba3a315cb7de2207fc933e34c948
OrcunGokoluk <orcun.gokoluk@outlook.com>
2025-05-01 14:52:43 +0300

    go-to-last-page-button

--- src/Pages/SearchPage.jsx
+++ src/Pages/SearchPage.jsx
@@ -6,10 +6,15 @@ import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 
 
 function SearchPage() {
+
+  //States
     const [movieData,setMovieData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages]=useState(0);
+
+  //Params
     const [searchParams]= useSearchParams();
 
-    const [currentPage, setCurrentPage] = useState(1);
 
     const navigate= useNavigate();
     const query = searchParams.get("query");
@@ -23,7 +28,6 @@ function SearchPage() {
     }
 
     useEffect(()=>{
-
         const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=${page ? page : currentPage}&include_adult=false`
 
         fetch(url)
@@ -32,16 +36,14 @@ function SearchPage() {
             throw new Error("Error: ", res.status);
           }
            return res.json()}
-        )
-
-        .then(data=>{
+        ).then(data=>{
           if (page) {
             setCurrentPage(page);
           }
           setMovieData(data.results)
-          console.log(data.results)
-        })
-        .catch(error=>
+          setTotalPages(data.total_pages)
+          console.log(data)
+        }).catch(error=>
           console.error(error))
     },[query, page])
 
@@ -57,59 +59,82 @@ function SearchPage() {
     
   return (
     <>
+    {/* Form */}
+
       <form action={getData} className='input-wrapper'>
           <button  className='search-icon'><FaMagnifyingGlass  /></button>
           <input className='searchbar' type="text" name="searchbar" placeholder='Search'/>
       </form>
       <h2 className='search-results-text'>Search Results</h2>
+
+    {/*Not Found */}
+
        {movieData.length<1 ? <h2 className='notFound'>Oops! No movies found. Try checking the spelling or searching for something else.</h2>:""}
+
+    {/*Movie Cards */}
+
       {movieData ? movieData.map((movie)=> <MovieCard key={movie.id} id={movie.id} title={movie.title} image_path={movie.poster_path} date={movie.release_date} overview={movie.overview}/> ):""}
-    <Pagination className='pagination-container'>
-      <PaginationItem>
-        <PaginationLink
-          first
-          href="#"
-        />
-      </PaginationItem>
-      <NavLink to={genNewSearchParamString("page", Number(currentPage)-1)}>
-          <PaginationItem>
-            <PaginationLink previous/>
-          </PaginationItem>
+
+    {/*Pagination */}
+
+      <Pagination className='pagination-container'>
+
+        {/* << */}
+      <NavLink to={genNewSearchParamString("page", "1")}>
+        <PaginationItem>
+          <PaginationLink
+            first
+            href="#"
+          />
+        </PaginationItem>
       </NavLink>
-      <NavLink to={genNewSearchParamString("page", currentPage)}>
-          <PaginationItem>
-            <PaginationLink>
-            {`${currentPage}`}
-            </PaginationLink>
-          </PaginationItem>
+        {/* < */}
+        <NavLink to={genNewSearchParamString("page", Number(currentPage)-1)}>
+            <PaginationItem>
+              <PaginationLink previous/>
+            </PaginationItem>
         </NavLink>
-        <NavLink to={genNewSearchParamString("page", Number(currentPage)+1)}>
-          <PaginationItem>
-            <PaginationLink>
-            {`${Number(currentPage)+1}`}
-            </PaginationLink>
-          </PaginationItem>
-        </NavLink>
-        <NavLink to={genNewSearchParamString("page", Number(currentPage)+2)}>
-          <PaginationItem>
-            <PaginationLink>
-            {`${Number(currentPage)+2}`}
-            </PaginationLink>
-          </PaginationItem>
-        </NavLink>
-        <NavLink to={genNewSearchParamString("page", Number(currentPage)+1)}>
-          <PaginationItem>
-            <PaginationLink next/>
-          </PaginationItem>
-      </NavLink>
-      <PaginationItem>
-        <PaginationLink
-          href="#"
-          last
-        />
-      </PaginationItem>
-    </Pagination>
-    </>
+        {/* - */}
+        <NavLink to={genNewSearchParamString("page", Number(currentPage)-1)}>
+            <PaginationItem>
+              <PaginationLink>
+              {`${currentPage}`}
+              </PaginationLink>
+            </PaginationItem>
+          </NavLink>
+        {/* current */}
+          <NavLink  to={genNewSearchParamString("page", currentPage)}>
+            <PaginationItem>
+              <PaginationLink>
+              {`${Number(currentPage)+1}`}
+              </PaginationLink>
+            </PaginationItem>
+          </NavLink>
+        {/* + */}
+          <NavLink to={genNewSearchParamString("page", Number(currentPage)+2)}>
+            <PaginationItem>
+              <PaginationLink>
+              {`${Number(currentPage)+2}`}
+              </PaginationLink>
+            </PaginationItem>
+          </NavLink>
+        {/* > */}
+          <NavLink to={genNewSearchParamString("page", Number(currentPage)+1)}>
+              <PaginationItem>
+                <PaginationLink next/>
+              </PaginationItem>
+          </NavLink>
+        {/* >> */}
+          <NavLink to={genNewSearchParamString("page", totalPages)}>
+            <PaginationItem>
+              <PaginationLink
+                href="#"
+                last
+              />
+            </PaginationItem>
+          </NavLink>
+      </Pagination>
+      </>
   )
 }
 
