4d04ca145356abf00878813f422b48cc23be5075
OrcunGokoluk <orcun.gokoluk@outlook.com>
2025-05-05 13:11:31 +0300

    add-desktop-menu

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -5,9 +5,13 @@ import '../CSS/main-layout.css'
 import { Outlet, Link } from 'react-router-dom';
 import { IoArrowBackSharp } from "react-icons/io5";
 import {NavLink} from "react-router-dom";
+import { MdKeyboardArrowDown } from "react-icons/md";
 function MainLayout() {
 
  const [toggleMenu, setToggleMenu] = useState(false);
+
+ const [toggleMovieMenu, setToggleMovieMenu] = useState(true);
+ const [toggleTVeMenu, setToggleTVMenu] = useState(true);
 
   return (
     <>
@@ -16,7 +20,43 @@ function MainLayout() {
         <div className='hamburger-menu-logo'>
           <div onClick={()=>{setToggleMenu(prev=>!prev)}}><RxHamburgerMenu/></div>
         </div>
-        <h1><span className='spn-logo'>Cine</span>List</h1>
+        <nav className="desktop-navigation">
+        <h1><span className='spn-logo'>Cine</span>List</h1> 
+         <section className='main-menu-categories'>
+              <div className='nav-movie-container' onMouseEnter={()=>setToggleMovieMenu(false)} onMouseLeave={()=>setToggleMovieMenu(true)}>Movies <MdKeyboardArrowDown className='cinelist-orange'/>
+                <ul className={`navigation-list ${toggleMovieMenu?"hide-movie-categories":""}`}>
+                  <li>
+                    <NavLink to="movie">Popular</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="movie/top-rated">Top Rated</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="movie/upcoming">Upcoming</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="movie/new-playing">New Playing</NavLink>
+                  </li>
+              </ul>
+              </div>
+              <div className='nav-tv-container' onMouseEnter={()=>setToggleTVMenu(false)} onMouseLeave={()=>setToggleTVMenu(true)}>TV Shows <MdKeyboardArrowDown className='cinelist-orange'/>
+              <ul className={`navigation-list ${toggleTVeMenu ? " hide-tv-categories":""}`}>
+                  <li>
+                    <NavLink to="tv">Popular</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="tv/top-rated">Top Rated</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="tv/upcoming">Upcoming</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="tv/airing-today">Airing Today</NavLink>
+                  </li>
+              </ul>
+              </div>
+         </section>
+        </nav>
         <div className='account-icon'>
          <Link to="login"> <IoMdPerson/> </Link>
         </div>
